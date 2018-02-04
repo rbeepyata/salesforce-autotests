204dec22d3e7670a222498959428cebb33f88f5e
Raghunandan Beepyata <Raghunandan_Beepyata@epam.com>
2018-02-05 04:02:15 +0530

    Test

--- src/main/java/com/edmunds/tests/Rough.java
+++ src/main/java/com/edmunds/tests/Rough.java
@@ -1,18 +1,40 @@
 package com.edmunds.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
 import com.epam.automation.core.objectrepository.ExcelObjectRepository;
 
 public class Rough {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		ExcelObjectRepository xor = new ExcelObjectRepository();
-		xor.loadObjectRepository("src\\main\\resources\\or\\locators.xlsx");
 		
-		System.out.println(xor.getRepo().get("com.edmunds.pages.LoginPage.Username").getLocator().getLocatorValue());
+	}
+	
+	@Test
+	public void test() throws InterruptedException{
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "lib//drivers//linux//chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--start-maximized");
+	    chromeOptions.addArguments("disable-infobars");
+	    Map<String, Object> prefs = new HashMap<String, Object>();
+	    prefs.put("credentials_enable_service", false);
+	    prefs.put("profile.password_manager_enabled", false);
+	    chromeOptions.setExperimentalOption("prefs", prefs);
+		driver = new ChromeDriver(chromeOptions);	
 		
-				
+		driver.get("http://google.com");
 		
+		Thread.sleep(5000);
+		
+		driver.close();
 	}
 
 }
